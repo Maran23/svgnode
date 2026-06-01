2bb1e99fbf1fcc4bdacfd30035c87623d88442b7
Marius Hanl <mariushanl@web.de>
2026-06-01 19:54:02 +0200

    Initial SVGNode

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/// Defines all components that are required and available in SVGNode.
+///
+/// @author Marius Hanl
+module tools.maran.svgnode {
+    requires javafx.graphics;
+
+    exports tools.maran.svgnode;
+}
---
+++ src/test/java/tools/maran/svgnode/Launcher.java
@@ -0,0 +1,8 @@
+package tools.maran.svgnode;
+
+public class Launcher {
+
+    static void main() {
+        SvgNodeManualTest.main(new String[0]);
+    }
+}
